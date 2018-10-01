0e90954aa23baf935566a8ca44916416385e17b8
Valentin Thirion <vt@abakusitsolutions.eu>
2018-10-01 23:02:50 +0200

    [ADD] Added some domains [BUG] Fixed old 'team_category'

--- racy/models/racer.py
+++ racy/models/racer.py
@@ -11,7 +11,7 @@ class Racer(models.Model):
     name = fields.Char(string='Name', copy=False, index=True, required=True)
     race_id = fields.Many2one('racy.race', string='Race', required=True)
     number = fields.Integer(string="Number")
-    category_id = fields.Many2one('racy.team_category', string='Category')
+    category_id = fields.Many2one('racy.category', string='Category')
     partner_id = fields.Many2one('res.partner', string="Contact")
     lap_ids = fields.One2many('racy.lap', 'racer_id', string="Laps")
     route_id = fields.Many2one('racy.route', string="Route", required=True)
