7b66a186e381993f82ef59257ca4b8fce0a5d14b
Valentin Thirion <vt@abakusitsolutions.eu>
2018-10-02 14:30:38 +0200

    [ADD] Groups management improvement

--- racy/models/group.py
+++ racy/models/group.py
@@ -10,3 +10,9 @@ class Group(models.Model):
 
     name = fields.Char(string='Name', copy=False, index=True, required=True)
     race_id = fields.Many2one('racy.race', string='Race', required=True)
+
+    team_count = fields.Integer(string="Team count", compute='_compute_team_count')
+
+    @api.one
+    def _compute_team_count(self):
+        self.team_count = len(self.env['racy.team'].search([('group_id', '=', self.id)]))
