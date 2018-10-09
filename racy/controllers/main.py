e01f0298ef7a03bd7240f48aa05ab3fd08aeec89
Valentin Thirion <valentinthirion@gmail.com>
2018-10-09 23:26:15 +0200

    [ADD] AJAX code and controller for passing number

--- racy/controllers/main.py
+++ racy/controllers/main.py
@@ -10,8 +10,53 @@ _logger = logging.getLogger(__name__)
 class RacyCountPage(http.Controller):
 
     @http.route([
-        '/racy/race/<model("racy.race"):race>',
-    ], type='http', auth="public", website=True)
-    def racy_race_page(self, page=0, race=None, search='', ppg=False, **post):
-        race_ids = http.request.env['racy.race'].sudo().search([('id', '=', race.id)])
+        '/racy/race/<model("racy.race"):race_id>',
+    ], type='http', auth="user", website=True)
+    def racy_race_page(self, page=0, race_id=None, search='', ppg=False, **post):
+        race_ids = http.request.env['racy.race'].sudo().search([('id', '=', race_id.id)])
         return http.request.render('racy.racy_race_count_page', {'race': race_ids})
+
+    @http.route([
+        '/racy/number_passed/validate_json'],
+                type='json', auth="user", methods=['POST'], website=True, csrf=False)
+    def number_passed_in_race(self, race_id, number, *kw):
+        """ Ajax call to create a lap for a passed number """
+        _logger.debug("\n\n HELLO WORLD !\n\n")
+        # Test if team or racer
+        if race_id and number:
+            race = request.env['racy.race'].sudo().search([('id', '=', race_id.id)], limit=1)
+            if race.racing_mode == 'single':
+                racer = request.env['racy.racer'].sudo().search([
+                    ('race_id', '=', race_id.id),
+                    ('number', '=', int(number)),
+                    ], limit=1)
+                if not racer or len(racer) != 1:
+                    return {
+                        'racing_mode': 'single',
+                        'racer_passed_id': -1
+                    }
+                else:
+                    return {
+                        'racing_mode': 'single',
+                        'racer_passed_id': racer.id,
+                        'racer_passed_number': racer.number,
+                        'racer_passed_name': racer.name,
+                    }
+            elif race.racing_mode == 'team':
+                team = request.env['racy.team'].sudo().search([
+                    ('race_id', '=', race_id.id),
+                    ('number', '=', int(number)),
+                    ], limit=1)
+                if not team or len(team) != 1:
+                    return {
+                        'racing_mode': 'team',
+                        'team_passed_id': -1
+                    }
+                else:
+                    return {
+                        'racing_mode': 'team',
+                        'team_passed_id': team.id,
+                        'team_passed_number': team.number,
+                        'team_passed_name': team.name,
+                        'team_passed_lap_count': team.lap_count}
+
